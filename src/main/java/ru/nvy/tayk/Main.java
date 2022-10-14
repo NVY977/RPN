b1103309817e3421b10436b18b744ac2617dce37
Nickolay Yazykov <80327747+NVY977@users.noreply.github.com>
2022-10-14 18:04:18 +0300

    Update Main.java

--- src/main/java/ru/nvy/tayk/Main.java
+++ src/main/java/ru/nvy/tayk/Main.java
@@ -4,12 +4,10 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        while (true) {
             Scanner in = new Scanner(System.in);
             System.out.print("Input expression: ");
             String str = in.nextLine();
             System.out.println(str);
             System.out.println(Calculator.solve(str));
-        }
     }
 }
